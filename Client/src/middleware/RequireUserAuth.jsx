c0ba30cc803cdf4022666d30c1d52450a0b6795d
NguyenToanThang-kotaru <thangkun07@gmail.com>
2025-10-14 19:02:51 +0700

    GUI Phu Huynh

--- Client/src/middleware/RequireUserAuth.jsx
+++ Client/src/middleware/RequireUserAuth.jsx
@@ -1,11 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function RequireAdminAuth() {
-  const isAdmin = sessionStorage.getItem("isAdmin");
+  const isLogined = sessionStorage.getItem("isUser");
   const accessToken = sessionStorage.getItem("accessToken");
 
-  if (!isAdmin || !accessToken ) {
-    return <Navigate to="/admin/login" replace />;
-  }
+  // if (!isAdmin || !accessToken ) {
+  //   return <Navigate to="/admin/login" replace />;
+  // }
   return <Outlet />;
 }
